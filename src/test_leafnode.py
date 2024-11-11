5315fbb7dd87ed22e60601a9db84d1a1e77e160c
Vegard Seines <vegsei@gmail.com>
2024-11-11 15:43:06 +0100

    add leafnode to represent a html leaf

---
+++ src/test_leafnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leafnode_renders_simple_text(self):
+        node = LeafNode(tag=None, value="simple text")
+        self.assertEqual(node.to_html(), "simple text")
+
+    def test_leafnode_renders_paragraph(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+
+    def test_leafnode_renders_link_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_empty_leaf_value_raises(self):
+        node = LeafNode(None, None)  # pyright: ignore
+        self.assertRaises(ValueError, node.to_html)
+
+
+if __name__ == "__main__":
+    unittest.main()
